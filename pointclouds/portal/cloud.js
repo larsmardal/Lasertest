d27bbcd37171c055cbdab2f48aabbdf036697fa7
larsmardal <larsmardal@Larss-MacBook-Pro.local>
2016-09-15 21:35:40 +0200

    Lasertest
    sldfsdf

---
+++ pointclouds/portal/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 11204990,
+    "boundingBox": {
+        "lx": 619200.0,
+        "ly": 6966000.0,
+        "lz": 400.75,
+        "ux": 620000.0,
+        "uy": 6966800.0,
+        "uz": 1200.75
+    },
+    "tightBoundingBox": {
+        "lx": 619200.0,
+        "ly": 6966000.0,
+        "lz": 400.75,
+        "ux": 620000.0,
+        "uy": 6966600.0,
+        "uz": 483.57
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 6.928203105926514,
+    "scale": 0.01,
+    "hierarchyStepSize": 5
+}
